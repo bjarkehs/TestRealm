f989b7a07cd0ffb4b20379bbbbb3733da8e98356
Bjarke Hesthaven Søndergaard <bhsgaard@gmail.com>
2017-01-19 11:55:15 +0100

    Initial Commit

---
+++ TestRealm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestRealm
+//
+//  Created by Bjarke Hesthaven Søndergaard on 19/01/2017.
+//  Copyright © 2017 Bjarke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
